a63d9078cba98224c5cf1a61ed8c33d104f860b0
Matt Galvin <matthewgalvin908@gmail.com>
2013-05-08 19:47:35 -0400

    added login form, javscript,  biotch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,3 @@
 //= require 'custom.modernizr.js'
 //= require 'foundation.min.js'
 //= require_tree .
-function cancel_user_form()
-{
-  $('#create_user_form').empty();
-}
--- app/assets/javascripts/splash.js
+++ app/assets/javascripts/splash.js
@@ -2,3 +2,7 @@ function cancel_user_form()
 {
   $('#create_user_form').empty();
 }
+function cancel_login_form()
+{
+  $('#login_form').empty();
+}
